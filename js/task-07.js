7d6e58a8f04d139d8b6e5fc6800bea50191ad12f
eche-unicorn <d.anasteysha@gmail.com>
2021-08-15 20:18:50 +0300

    task 7 completed

---
+++ js/task-07.js
@@ -0,0 +1,12 @@
+const getInput = document.querySelector('#font-size-control');
+const getSpan = document.querySelector('#text');
+
+console.log(getInput);
+console.log(getSpan.textContent);
+
+getInput.addEventListener('change', changeSize);
+
+function changeSize(event) {
+  let currentFontSize = event.target.value;
+  getSpan.style.fontSize = `${currentFontSize}px`;
+}
